54d81bcbfc817c7adb9090b2bceef90bb30acf32
Do Chau Long <dolong2110@gmail.com>
2022-09-23 13:29:14 +0700

    create routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import './App.scss';
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
 
-const App = () => ("Hello")
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+        </Route>
+      </Routes>
+    </>
+  )
+}
 
 export default App;
