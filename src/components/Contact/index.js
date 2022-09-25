60a3d792235526636390817a30e7e4ff3847e75d
Do Chau Long <dolong2110@gmail.com>
2022-09-25 14:17:59 +0700

    init contact page

---
+++ src/components/Contact/index.js
@@ -0,0 +1,43 @@
+import './index.scss'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, []);
+
+  return (
+    <>
+      <div className='container contact-page'>
+        <div className='text-zone'>
+          <h1>
+            <AnimatedLetters letterClass={letterClass} strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']} idx={15} />
+          </h1>
+          <p>
+            I am interested in freelance opportunities - especially ambitious on
+            large projects. However, if you have other request or question, please
+            don't hesitate to contact me using below form either.
+          </p>
+          <div className='contact-form'>
+            <form>
+              <ul>
+                <li className='half'>
+
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Loader type='pacman' active/>
+    </>
+  )
+}
+
+export default Contact
